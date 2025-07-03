e3e6ac2c52f36b27073b533ca27a82985d870981
AshtonautEdu <jzqq6560@leeds.ac.uk>
2025-07-03 17:22:34 +0100

    Add printing of processor number (and group) in Windows

--- aire-hello-world/hello_world_threaded.py
+++ aire-hello-world/hello_world_threaded.py
@@ -1,7 +1,22 @@
 from mpi4py import MPI
+import ctypes
+from ctypes import wintypes
 
 world_comm = MPI.COMM_WORLD
 world_size = world_comm.Get_size()
 rank = world_comm.Get_rank()
 
+print("MPI Details:")
 print(f'World Size: {world_size}  Rank: {rank}')
+
+class PROCESSOR_NUMBER(ctypes.Structure):
+    _fields_ = [("Group", wintypes.WORD),
+                ("Number", wintypes.BYTE),
+                ("Reserved", wintypes.BYTE)]
+    
+pn = PROCESSOR_NUMBER()
+
+ctypes.windll.kernel32.GetCurrentProcessorNumberEx(ctypes.byref(pn))
+
+print("Processor Details:")
+print(f'Group: {pn.Group}  Number: {pn.Number}\n')
