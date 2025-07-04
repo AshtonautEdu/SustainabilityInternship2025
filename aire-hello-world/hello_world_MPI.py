81dbd2f922c04a4f15ef4927b6e022cf550bc0f1
AshtonautEdu <jzqq6560@leeds.ac.uk>
2025-07-04 11:42:19 +0100

    Add printing of processor number for linux

--- aire-hello-world/hello_world_MPI.py
+++ aire-hello-world/hello_world_MPI.py
@@ -1,6 +1,8 @@
-from mpi4py import MPI
 import ctypes
 from ctypes import wintypes
+import os
+
+from mpi4py import MPI
 
 world_comm = MPI.COMM_WORLD
 world_size = world_comm.Get_size()
@@ -9,14 +11,19 @@ rank = world_comm.Get_rank()
 print("MPI Details:")
 print(f'World Size: {world_size}  Rank: {rank}')
 
-class PROCESSOR_NUMBER(ctypes.Structure):
-    _fields_ = [("Group", wintypes.WORD),
-                ("Number", wintypes.BYTE),
-                ("Reserved", wintypes.BYTE)]
-    
-pn = PROCESSOR_NUMBER()
-
-ctypes.windll.kernel32.GetCurrentProcessorNumberEx(ctypes.byref(pn))
-
 print("Processor Details:")
-print(f'Group: {pn.Group}  Number: {pn.Number}\n')
+
+name = os.name
+if (name == "nt"):
+    class PROCESSOR_NUMBER(ctypes.Structure):
+        _fields_ = [("Group", wintypes.WORD),
+            ("Number", wintypes.BYTE),
+            ("Reserved", wintypes.BYTE)]
+    
+    pn = PROCESSOR_NUMBER()
+    ctypes.windll.kernel32.GetCurrentProcessorNumberEx(ctypes.byref(pn))
+    
+    print(f'Group: {pn.Group}  Number: {pn.Number}\n')
+elif (name == "posix"):
+    libc = ctypes.CDLL("libc.so.6")
+    print(f'Number: {libc.sched_getcpu()}')
