d3f619a0a5f27c32076421b573df33a2f14a9976
AshtonautEdu <jzqq6560@leeds.ac.uk>
2025-07-23 17:20:48 +0100

    Attempt to use CXXFLAGS for GROMACS

--- phoronix/scripts/test-scripts/basic-flag-test.sh
+++ phoronix/scripts/test-scripts/basic-flag-test.sh
@@ -2,8 +2,9 @@
 # This file was used to try out the Phoronix Test Suite with different compiler flags.
 # It worked for SciMark2, which will happily be modified by the CFLAGS environment variable.
 # It did not work for GROMACS.
+# Tried CXXFLAGS, still no joy. I need to edit the install.sh for the profile directly.
 
-benchmark=scimark2
+benchmark=local/gromacs-test
 flag_list=(
 	"-O0"
 	"-O1"
@@ -13,5 +14,9 @@ flag_list=(
 test_results_name="Basic Flag Test"
 
 for flag in "${flag_list[@]}"; do
-	CFLAGS="$flag" TEST_RESULTS_IDENTIFIER="$flag" TEST_RESULTS_NAME="$test_results_name" phoronix-test-suite benchmark $benchmark
+	export CFLAGS="$flag"
+        export CXXFLAGS="$flag"	
+	export TEST_RESULTS_IDENTIFIER="$flag" 
+	export TEST_RESULTS_NAME="$test_results_name" 
+	phoronix-test-suite benchmark $benchmark
 done
